4e9a8494f5ee86e7121f6f43d8ed816aafbb4f01
Math1720182 <biclou17@gmail.com>
2026-08-15 21:10:31 +0200

    Update GitHub link in analyse.py

--- app/analyse.py
+++ app/analyse.py
@@ -136,7 +136,7 @@ with st.sidebar:
     """,)
     
     st.caption('Made with ❤️ by Thomas')
-    st.link_button('Voir le code sur GitHub 👾', "https://github.com/Math1720182/dvf_2021_2025_analysis")
+    st.link_button('Voir le code sur GitHub 👾', "https://github.com/Math1720182/Mon-Analyse-Immo-App")
 
     
 #---------------------------------------------------
